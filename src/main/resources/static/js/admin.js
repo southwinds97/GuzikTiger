8ece5722cd35928e5d31ff65f300c85f5f333e23
southwinds97 <southwinds970521@gmail.com>
2024-10-02 18:19:54 +0900

    구직타이거 채팅관리자 추가

--- src/main/resources/static/js/admin.js
+++ src/main/resources/static/js/admin.js
@@ -1,29 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-    // 상품 편집 버튼 클릭 시 처리할 로직
-    const editButtons = document.querySelectorAll("button");
-
-    editButtons.forEach(button => {
-        button.addEventListener("click", () => {
-            alert("상품을 편집합니다.");
-            // 편집 로직 추가
-        });
-    });
+	// 상품 편집 버튼 클릭 시 처리할 로직
+	document.body.addEventListener("click", (event) => {
+		if (event.target.tagName === "BUTTON") {
+			alert("상품을 편집합니다.");
+			// 편집 로직 추가
+		}
+	});
 });
 
-const listId = {A:'productList', B:'orderList', C:'memberList'};
+const listId = { A: 'productList', B: 'orderList', C: 'memberList', D: 'chatList' };
 
-function listChk(id){
-	if(id===listId.A){
-		$('#'+listId.A).show();
-		$('#'+listId.B).hide();
-		$('#'+listId.C).hide();
-	}else if(id===listId.B){
-		$('#'+listId.A).hide();
-		$('#'+listId.B).show();
-		$('#'+listId.C).hide();
-	}else if(id===listId.C){
-		$('#'+listId.A).hide();
-		$('#'+listId.B).hide();
-		$('#'+listId.C).show();
-	}	
+function listChk(id) {
+	Object.values(listId).forEach(list => {
+		$('#' + list).hide();
+	});
+	$('#' + id).show();
 }
