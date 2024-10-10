dac4fbc9fb445f604f3ecf6b400304767f507112
southwinds97 <southwinds970521@gmail.com>
2024-10-10 13:08:12 +0900

    Flutter 연동 추가

--- src/main/java/com/edu/springboot/api/ApiController.java
+++ src/main/java/com/edu/springboot/api/ApiController.java
@@ -1,7 +1,9 @@
 package com.edu.springboot.api;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.edu.springboot.ParameterDTO;
@@ -16,6 +19,8 @@ import com.edu.springboot.member.IMemberService;
 import com.edu.springboot.member.MemberDTO;
 import com.edu.springboot.product.IProductService;
 import com.edu.springboot.product.ProductDTO;
+
+import utils.PagingUtil;
 
 @RestController
 @RequestMapping("api")
@@ -43,5 +48,27 @@ public class ApiController {
 	            return ResponseEntity.status(401).body(null);
 	        }
 	    }
+
+		@PostMapping("/category")
+		public ResponseEntity<List<ProductDTO>> category(@RequestBody Map<String, String> body) {
+			String searchKeyword = body.get("keyword");
+			// 검색 키워드를 기반으로 데이터를 조회합니다.
+			ArrayList<ProductDTO> lists;
+			String code = null;
+			int listArray = 2;
+			int offset = 0; // 기본값 설정
+			int limit = 10; // 기본값 설정
+		
+			Map<String, Object> params = new HashMap<>();
+			params.put("code", code);
+			params.put("list_array", String.valueOf(listArray));
+			params.put("offset", offset);
+			params.put("limit", limit);
+			params.put("searchKeyword", searchKeyword);
+		
+			lists = productDAO.getSelectByKeyword(params);
+		
+			return ResponseEntity.ok(lists);
+		}
 		
 }
